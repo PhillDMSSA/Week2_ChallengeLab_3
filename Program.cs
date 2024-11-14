b4ab756cb4b00314b7330c28e8b2eee2e57ee4c1
phillip deleon <phillipdeleon68@gmail.com>
2024-11-14 09:24:58 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Week2_ChallengeLab_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number: ");
+            int numLoop1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter another number: ");
+            int numLoop2 = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = numLoop2; i > 0; i--)
+            {
+                for (int j = 0; j < i; j++) 
+                {
+                    Console.Write(numLoop1); //ensure *Write()*
+                }
+                Console.WriteLine(); //moves to next line
+
+            }
+        }
+    }
+}
